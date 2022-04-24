8ac4ddb5cfa6328a54c0c7555bb4fe504d0c0046
Edgarcastrop <99065409+Edgarcastrop@users.noreply.github.com>
2022-04-23 19:38:25 -0600

    Add files via upload

---
+++ practicaComic/Comic.java
@@ -0,0 +1,355 @@
+  public class Comic{
+    public static void main(String[] args) {
+        Screen screen = new Screen();
+
+        screen.out("Amazing fantazy #15\n", "Helvetica", 20, Colors.RED);
+        screen.setVisible(true);
+        try {
+            Thread.sleep(2000);
+            screen.out("*En la escuela\n* Maestro: ¡A ver pandilla, nesecitamos a un chico mas para el baile!" +
+                    " ¿Que tal peter Parker allá atras?\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Alumno 1:¿Estas bromeando? ¡Ese raton de biblioteca no reconoce in Cha-Cha de un vals!\n",
+                    "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Alumna 2: ¿Peter Parker?¡EL es el unico timido de la secundaria Midtown\n",
+                    "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("*En casa*\n" +
+                    "Como se habran dado cuenta Peter Paker estaba lejos de ser el mas popular del campus! pero su tio" +
+                    " Ben pensaba que el era un muchacho muy especial\n", "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Tio Ben: ¡A mi no me engañas Petey! Se que estas despierto... ¡Y es hora de ir a la escuela!" +
+                    "\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: ¡Cielos tio Ben ¡Eres peor que un cuarto lleno de despertadores!\n",
+                    "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Mientras que para su tia May ¡Su sobrino era la luz de sus ojos!\n", "Helvetica",
+                    20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Tia May: Te prepare tu desayuno favorito Peter... ¡Panqueques de trigo!\n",
+                    "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Tio Ben: ¡No lo engordes mucho, querida! ¡Casi ya no puedo gararle en las luchas ahora!\n",
+                    "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("¡Los Profesores en la secundaria Mindtown tambien estaban maravillados del alumno estrella," +
+                    " trabajador y bien aseado!\n", "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Profesor Conors: ¡Manten el buen trabajo Parker y te aseguro que te esperara una beca" +
+                    " cuando te gradues!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: ¡Dare mi mejor esfuerzo senor \n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Sin embargo otros adolecentes pueden a veces, inconsientemente ser muy crueles con un" +
+                    " timido joven\n", "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("+Fuera de clase*\nPeter: Sally, yo he, ¿Me preguntaba si estabas ocupada esta noche...?\n", "Helvetica",
+                    20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Sally: Peter, por la milesima vex simplemente no eres mi tipo...\n", "Helvetica",
+                    20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Sally: ...Noo cuando los guapos como Flash Thompson estan cerca!\n", "Helvetica",
+                    20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Flash: ¡Admiro tu buen gusto muneca! ¡Pierdete cerebrito!\n", "Helvetica", 20,
+                    Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: Miren, hay una nueva exhibicion en la sala de ciencias esta noche! alguno de ustedes le" +
+                    " gustaria ir conmigo?\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Flash: ¡Salon de ciencias! ¡HAHAHAH!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Amigo de flash: ¡Quedate con la ciencia, tonto! ¿Nosotros con las chicas!\n",
+                    "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Si, para algunos, ser un adolecente tiene muchos momentos decepcionantes.\n",
+                    "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Amigo de flash: Nos vemos sabelotodo\n", "Helvetica", 20,
+                    Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Selly: Dale nuestros saludos a los golpeadores de atomo, Peter\n *Se van*", "Helvetica",
+                    20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: Algun dia les mostrare¡ Algun dia ellos lo lamentaran haberse reido de mi! " +
+                            "*entra al salon de ciencias*\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Unos minutos despues Peter olvida las burlas de sus compañeros mientras es transportado " +
+                    "hacia otro mundo....¡EL facinante mundo de la ciencia atomica\n", "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Profesor conors: Y ahora para la demostracion de como podemos controlar los rayos " +
+                    "radioactivos aqui en el laboratorio\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Pero mientras el experimento empieza, nadie se percata que una pequeña araña que deciende" +
+                    " el techo en un casi invisible hilo de red...\n", "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Una araña cuyo destino le ha dado un rol, aunque breve, protagonico en la drama que " +
+                    "llamamos vida\n", "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Accidentalmente absorbiendo una fantastica cantidad de radioactividad, el insecto morbido," +
+                    " en un repentino choque, muerde al ser viviente mas proximo, en el mismo segundo antes que se " +
+                    "escape la vida de su radioactivo cuerpo\n", "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(5000);
+            screen.out("Peter: ¡U-una araña! ¡Me pico! pero ¿Por que me esta quemando? ¿Por que esta brillando de esta " +
+                    "manera?\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: Mi cabeza... se siente algo extraña! ¡Ne-nesecito algo de aire!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Profesor conors: Parece que nuestro experimento asusto al joven Parker!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Sujeto cualquiera: ¡Que pena! ¡Debe de tener el estomago debil!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: ¿Que me esta pasando? me siento diferente com osi mi cuerpo entero estuviera cargado de" +
+                    " alguna clase de energia fantastica\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Envuelto en sus pensmientos, peter no escucha el auto que por poco lo arrolla, hasta el" +
+                    " preciso ultimo instante y luego inadvertido por los autos, pensarlo ¡pero que manera!\n",
+                    "Helvetica", 20, Colors.BLUE);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Sujeto en el auto: ¡Ese era un tonto que no volvera a soñar despierto cuando cruce la " +
+                    "calle!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Sijeto en el auto 2: ¡Te aseguro que si!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: ¡Pero que me pasa! ¡E-estoy trepando este muro tan facil como si caminara! \n",
+                    "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Niño: ¡Mami! ¡mira a aquel hombre repando por aquel lado del edificio!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Mama del niño: ¡Esta es la ultima pelicula de terror a la que te llevo jovencito!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: ¡Es increible alcance el techo en solo unos segundos!\n" +
+                    "Que es esto???¡Aplaste esta tuberia de acero como si estuviera hecha de papel!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: ¡Es la araña! ¡Tiene que ser! De alguna u otra forma.. en alguna milagrosa manera su" +
+                    " picadura ha transferido su propio poder.. ¡A mi!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("*Se cuelga de un cable*\nPeter: Puedo decender por este cable sin esfuerzo alguno como una" +
+                    " araña se colimpia en su propia red!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        try {
+            Thread.sleep(3500);
+            screen.out("Peter: ¡T-Tengo que buscarme tiempo para pensar! ¡Tengo que planear que puedo hacer con " +
+                    "esta increible habilidad que el destino me ha dado!\n", "Helvetica", 20, Colors.BLACK);
+            screen.setVisible(true);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+    }
+}
